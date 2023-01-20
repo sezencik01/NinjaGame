74c5004efb69bcf0ca24c4c0cae35db9137880d7
Ayşe Sezen Doğan <sezenxdogan1@hotmail.com>
2023-01-20 23:42:57 +0300

    fn
    fn

--- Assets/Scripts/StartMenuCloser.cs
+++ Assets/Scripts/StartMenuCloser.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartMenuCloser : MonoBehaviour
+    
 {
-    // Start is called before the first frame update
+    [SerializeField] private Image start;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+      
     }
 }
